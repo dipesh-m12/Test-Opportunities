7bdccfc92137dab10a17c82b3fc9c2698ab740c7
Dipesh <mavinash423@gmail.com>
2025-05-23 17:49:21 +0530

    hello

--- src/pages/ManageJobs.tsx
+++ src/pages/ManageJobs.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @typescript-eslint/no-unused-expressions */
 /* eslint-disable prefer-const */
 /* eslint-disable @typescript-eslint/no-explicit-any */
